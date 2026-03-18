59535b6ea02b8e65e6580c5d8f4beb063e944e88
Mohammed Faizan <mdfaizanansari1187@gmail.com>
2026-03-18 13:06:48 +0530

    Hello+Default msge

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,10 @@
-public class HelloWorld {
+public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // Check if argument is provided, else use default "World"
+        String name = (args.length > 0) ? args[0] : "World";
+
+        // Display greeting
+        System.out.println("Hello, " + name + "!");
     }
 }
